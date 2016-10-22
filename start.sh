6e6951166096517a303686c47040e7821019fa70
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-22 08:35:36 -0400

    Use smtp_helo_name instead of myhostname

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 mkdir -p /var/spool/postfix/etc
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 cp /etc/services /var/spool/postfix/etc/services
-sed -i s/{{NODE_HOSTNAME}}/${NODE_HOSTNAME}/g /etc/postfix/master.cf
+sed -i s/{{NODE_HOSTNAME}}/${NODE_HOSTNAME}/g /etc/postfix/main.cf
 rsyslogd
 postfix start
 tail -f /var/log/maillog
