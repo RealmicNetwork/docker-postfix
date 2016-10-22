532e1abbcb89b1b96439d8ae81e13aa7cff4fca4
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-22 08:31:14 -0400

    Sed the correct file

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 mkdir -p /var/spool/postfix/etc
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 cp /etc/services /var/spool/postfix/etc/services
-sed -i sed -i 's/{{NODE_HOSTNAME}}/${NODE_HOSTNAME}/g' index.html
+sed -i s/{{NODE_HOSTNAME}}/${NODE_HOSTNAME}/g /etc/postfix/master.cf
 rsyslogd
 postfix start
 tail -f /var/log/maillog
