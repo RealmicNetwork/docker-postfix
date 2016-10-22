94b98faaca0d4000dc3d419be87e3ecb9917fdef
Connor Catlett <ccatlett2000@mctherealm.net>
2016-10-22 08:22:05 -0400

    Use node's hostname for sending

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 mkdir -p /var/spool/postfix/etc
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 cp /etc/services /var/spool/postfix/etc/services
+sed -i sed -i 's/{{NODE_HOSTNAME}}/${NODE_HOSTNAME}/g' index.html
 rsyslogd
 postfix start
 tail -f /var/log/maillog
